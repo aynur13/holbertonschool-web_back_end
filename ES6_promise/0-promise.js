b3a1a0a1dd94c5fd8ca64f2dd2cbcef975fb0506
aynur13 <aynurmusayeva2007@gmail.com>
2025-04-09 06:43:37 +0000

    fix

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+const getResponseFromAPI = () => {
+  return new Promise((resolve, reject) => {
+	  resolve("Promise resolved successfully");
+  });
+}
+
+export default getResponseFromAPI;
